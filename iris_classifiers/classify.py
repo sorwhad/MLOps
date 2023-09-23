568145d0a9ced352bacb5f4dd6fc740dcb62ed36
Vadim Sorokin <vadimsorokin@Vadims-MacBook-Air.local>
2023-09-23 03:17:15 +0300

    initial commit

--- iris_classifiers/classify.py
+++
@@ -1,9 +0,0 @@
-from sklearn.svm import SVC
-
-def classify_iris(data, target, classifier='SVM'):
-    if classifier == 'SVM':
-        clf = SVC()
-        clf.fit(data, target)
-        return clf.predict(data) 
-    else: 
-        raise NotImplementedError("Other classifiers are not available yet.")
--- iris_classifiers/fetch_data.py
+++
@@ -1,5 +0,0 @@
-from sklearn import datasets
-
-def get_iris_data(): 
-    iris = datasets.load_iris()
-    return iris 
